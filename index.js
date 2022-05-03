247894ec8d85f71c78ce20b115b20a8202d3beea
Your Name <you@example.com>
2022-05-04 03:41:33 +0600

    api created for an id

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express()
 
@@ -21,6 +21,13 @@ async function laptop() {
             const cursor = laptopCollection.find(query)
             const laptops = await cursor.toArray();
             res.send(laptops)
+        })
+
+        app.get('/laptop/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const laptop = await laptopCollection.findOne(query)
+            res.send(laptop)
         })
 
     }
