453c40aa7fb67e5a0f5cc25e20b4a9d339ad50e3
Your Name <you@example.com>
2022-05-04 02:43:09 +0600

    save data shown creating a api

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express()
 
 app.use(cors())
 app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7cgfs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function laptop() {
+    try {
+        await client.connect();
+        const laptopCollection = client.db("laptop-house").collection("inventory");
+        app.get('/laptops', async (req, res) => {
+            const query = {};
+            const cursor = laptopCollection.find(query)
+            const laptops = await cursor.toArray();
+            res.send(laptops)
+        })
+
+    }
+    finally {
+
+    }
+}
+laptop().catch(console.dir)
+
 
 app.get('/', (req, res) => {
     res.send('Our server is running from heroku')
