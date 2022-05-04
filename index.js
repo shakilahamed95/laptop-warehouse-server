a83f22a6f7fb4418a1a4f759a89150c38a8b2026
Your Name <you@example.com>
2022-05-05 05:40:52 +0600

    delete api created

--- index.js
+++ index.js
@@ -61,6 +61,13 @@ async function laptop() {
 
         })
 
+        app.delete('/laptops/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await laptopCollection.deleteOne(query);
+            res.send(result);
+        })
+
     }
     finally {
 
