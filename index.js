77343e61138dc4da96cae0bf70599ae249ec717b
Your Name <you@example.com>
2022-05-08 21:54:11 +0600

    cors policy

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express()
 
-app.use(cors())
+const corsConfig = {
+    origin: true,
+    Credentials: true
+}
+app.use(cors(corsConfig))
+app.options('*', cors(corsConfig))
 app.use(express.json());
 
 
